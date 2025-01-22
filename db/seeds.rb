999d2825f894e4cab7006131d372d46e0c43c978
Luiza Lopes <t.luiza.lopes@fabrica.locaweb.com.br>
2025-01-22 15:46:34 -0300

    change the views of devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-user = User.create(email: "teste@teste.com", password: "123456")
+user = User.create(email: "admin@exemplo.com", password: "123456")
 
 post = Post.create(title: "Teste", content: "Teste", user: user)
 
